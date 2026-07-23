0d48412e3d5964b3f27f93562b2527ffc3be8e4a
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-23 13:47:29 -0300

    feat: creating TecData and importing Tec_images

---
+++ src/shared/constants/TecData.ts
@@ -0,0 +1,61 @@
+import {
+  Fastapi,
+  Python,
+  ReactIcon,
+  Supabase,
+  Tailwind,
+  Tanstack,
+  Axios,
+  Poetry,
+} from './Tec_images';
+
+export const tecData = [
+  {
+    id: 1,
+    icon: Fastapi,
+    title: 'FastAPI',
+    text: 'O núcleo velo que sustenta sistemas intergaláticos.',
+  },
+  {
+    id: 2,
+    icon: Python,
+    title: 'Python',
+    text: 'A energia inteligente que move toda a operação',
+  },
+  {
+    id: 3,
+    icon: ReactIcon,
+    title: 'React',
+    text: 'Interfaces vivas em constante expansão no universo.',
+  },
+  {
+    id: 4,
+    icon: Supabase,
+    title: 'Supabase',
+    text: 'Onde dados e autenticação orbitam em perfeita harmonia.',
+  },
+  {
+    id: 5,
+    icon: Tailwind,
+    title: 'Tailwind CSS',
+    text: 'A estrurura estética que molda universos digitais.',
+  },
+  {
+    id: 6,
+    icon: Tanstack,
+    title: 'Tanstack Query',
+    text: 'Fluxos de dados sincronizados além do espaço-tempo.',
+  },
+  {
+    id: 7,
+    icon: Axios,
+    title: 'Axios',
+    text: 'Conexões rápidas atravessando qualquer dimensão.',
+  },
+  {
+    id: 4,
+    icon: Poetry,
+    title: 'Poetry',
+    text: 'Orquestrando dependências com precisão cósmica.',
+  },
+];
