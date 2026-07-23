18b084ed13fce442d48e8d2a77a1b2c0e493ea1d
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-23 13:43:56 -0300

    Feat: creating a way to share all icons

---
+++ src/shared/constants/Tec_images.ts
@@ -0,0 +1,23 @@
+import star from '../../assets/Tecs_icons/icons/star.png';
+import Axios from '../../assets/Tecs_icons/icons/axios.png';
+import Fastapi from '../../assets/Tecs_icons/icons/fastapi.png';
+import Poetry from '../../assets/Tecs_icons/icons/poetry.png';
+import Python from '../../assets/Tecs_icons/icons/python.png';
+import ReactIcon from '../../assets/Tecs_icons/icons/react.png';
+import Supabase from '../../assets/Tecs_icons/icons/supabase.png';
+import Tailwind from '../../assets/Tecs_icons/icons/tailwind.png';
+import Tanstack from '../../assets/Tecs_icons/icons/tanstack.png';
+import BgDesktop from '../../assets/Tecs_icons/tec_bg/bg-desktop.png';
+
+export {
+  star,
+  Axios,
+  Fastapi,
+  Poetry,
+  Python,
+  ReactIcon,
+  Supabase,
+  Tailwind,
+  Tanstack,
+  BgDesktop,
+};
