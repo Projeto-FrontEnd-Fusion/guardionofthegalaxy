7bd02d7c19dca377aef292fe4a4baa383c8ced8d
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-23 13:49:04 -0300

    feat: creating MiniCard adding style and importing CardPropType

---
+++ src/components/MiniCard.tsx
@@ -0,0 +1,19 @@
+import type { CardPropType } from '@/shared/types/MiniCardtype';
+
+const MiniCard = ({ icon, alt, size, title, text }: CardPropType) => {
+  return (
+    <article className="flex flex-col border p-5.5 xl:p-5  border-[#B919BC] cursor-default bg-[#140C28] w-81.25 h-55.75 shadow-miniCard rounded-xl xl:w-65 xl:h-55.75">
+      <div className="flex flex-col items-center gap-7 justify-center">
+        <div className="bg-[#B919BC] w-10 h-10 rounded-[10px] mt-2.5 p-1.5">
+          <img src={icon} alt={`icon-${alt}`} className={size} />
+        </div>
+        <h3 className="text-white font-chakra font-semibold ">{title}</h3>
+      </div>
+      <p className="text-[#80D4DF] text-[14px] font-inter font-semibold pt-2 px-1 xl:px-0 tracking-wide xl:tracking-tighter">
+        {text}
+      </p>
+    </article>
+  );
+};
+
+export default MiniCard;
