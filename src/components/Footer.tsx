7bebb5763ec36a6fb0f91d9e1e12f1ca83b248d7
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-25 14:23:21 -0300

    style: change size of the icons ,height and text size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import SocialLink from './SocialLink';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0A0B12] h-[6.25em] lg:h-[8.5em] border-t border-[#B919BC] flex flex-row items-center justify-center gap-x-5 xl:gap-x-6">
-      <p className="text-[#B919BC] font-medium xl:font-extrabold font-roboto text-[10px] xl:text-[1em] cursor-default ">
+    <footer className="bg-[#0A0B12] h-[6.25em] lg:h-[7.5em] border-t border-[#B919BC] flex flex-row items-center justify-center gap-x-5 xl:gap-x-6">
+      <p className="text-[#B919BC] font-medium xl:font-extrabold font-roboto text-[10px] lg:text-[1em] xl:text-[1.1em] cursor-default ">
         Siga as redes sociais da comunidade Frontend Fusion
       </p>
       <div className="flex flex-row items-center gap-x-2.5 mt-2 xl:mt-0 xl:gap-x-5">
@@ -14,7 +14,7 @@ const Footer = () => {
             alt={alt}
             icon={icon}
             link={link}
-            size="w-7 xl:w-10"
+            size="w-[1.5em] lg:w-12"
           />
         ))}
       </div>
