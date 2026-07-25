8dfca0bcf8cd78fa7d5f31485f091e6c09203964
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-25 15:15:40 -0300

    style: change padding to p-[0.2em]

--- src/components/MiniCard.tsx
+++ src/components/MiniCard.tsx
@@ -4,7 +4,7 @@ const MiniCard = ({ icon, alt, size, title, text }: CardPropType) => {
   return (
     <article className="flex flex-col border p-5.5 xl:p-5  border-[#B919BC] cursor-default bg-[#140C28] w-81.25 h-55.75 shadow-miniCard rounded-xl xl:w-[254px] xl:h-[223px]">
       <div className="flex flex-col items-center gap-7 justify-center">
-        <div className="bg-[#B919BC] w-10 h-10 rounded-[10px] mt-2.5 p-1.5">
+        <div className="bg-[#B919BC] w-10 h-10 rounded-[10px] mt-2.5 p-[0.2em]">
           <img src={icon} alt={`icon-${alt}`} className={size} />
         </div>
         <h3 className="text-white font-chakra font-semibold ">{title}</h3>
