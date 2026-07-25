c3e5ef4faaf80a54973e1df866bc784ae7e6d365
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-25 18:28:11 -0300

    docs: moving Tec_images to assets/tecs_icons

---
+++ src/assets/Tecs_icons/Tec_images.ts
@@ -0,0 +1,23 @@
+import star from '../Tecs_icons/icons/star.png';
+import Axios from '../Tecs_icons/icons/axios.png';
+import Fastapi from '../Tecs_icons/icons/fastapi.png';
+import Poetry from '../Tecs_icons/icons/poetry.png';
+import Python from '../Tecs_icons/icons/python.png';
+import ReactIcon from '../Tecs_icons/icons/react.png';
+import Supabase from '../Tecs_icons/icons/supabase.png';
+import Tailwind from '../Tecs_icons/icons/tailwind.png';
+import Tanstack from '../Tecs_icons/icons/tanstack.png';
+import Bg from '../Tecs_icons/tec_bg/bg-desktop.png';
+
+export {
+  star,
+  Axios,
+  Fastapi,
+  Poetry,
+  Python,
+  ReactIcon,
+  Supabase,
+  Tailwind,
+  Tanstack,
+  Bg,
+};
