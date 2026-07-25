81e620ccaa2bca98cbf576afb89ba67ec687e92a
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-25 14:48:33 -0300

    style: change bg and change Bg name from BgDesktop to Bg

--- src/components/TecSection.tsx
+++ src/components/TecSection.tsx
@@ -1,12 +1,12 @@
 import MiniCard from './MiniCard';
-import { star, BgDesktop } from '../shared/constants/Tec_images';
+import { star, Bg } from '../shared/constants/Tec_images';
 import { tecData } from '@/shared/constants/TecData';
 
 const TecSection = () => {
   return (
     <section
       className="md:h-[100vhd] flex flex-col md:bg-cover md:bg-center px-5 py-10 md:p-0"
-      style={{ background: `url(${BgDesktop})` }}
+      style={{ background: `url(${Bg})` }}
     >
       <div className='flex xl:py-10 py-5 flex-col items-center justify-center bg-center bg-[url("./assets/Tecs_icons/tec_bg/bg-desktop.png")] md:bg-none '>
         <div className="flex flex-row items-center py-10 xl:py-15">
@@ -16,13 +16,13 @@ const TecSection = () => {
           </h2>
         </div>
 
-        <div className="flex flex-col items-center justify-center xl:scale-90 md:flex-row md:w-[90%] xl:w-[85%] flex-wrap gap-5">
+        <div className="flex flex-col items-center justify-center xl:scale-90 md:flex-row md:w-[90%] xl:w-[85%] flex-wrap gap-5 lg:gap-y-12.5">
           {tecData.map((item) => (
             <MiniCard
               key={item.id}
               icon={item.icon}
               alt={item.title}
-              size="w-9 xl:w-8"
+              size="w-9 lg:w-10"
               title={item.title}
               text={item.text}
             />
