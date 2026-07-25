4bcc7fc0699309f6e6816a23b88dc3cfab20c9fc
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-25 18:34:26 -0300

    refactor: change images route and style

--- src/components/TecSection.tsx
+++ src/components/TecSection.tsx
@@ -1,22 +1,22 @@
 import MiniCard from './MiniCard';
-import { star, Bg } from '../shared/constants/Tec_images';
+import { star, Bg } from '../assets/Tecs_icons/Tec_images';
 import { tecData } from '@/shared/constants/TecData';
 
 const TecSection = () => {
   return (
     <section
-      className="md:h-[100vhd] flex flex-col md:bg-cover md:bg-center px-5 py-10 md:p-0"
-      style={{ background: `url(${Bg})` }}
+      className="md:h-[100vhd]  flex flex-col bg-[#140C28] md:bg-cover md:bg-center px-5 py-10 md:p-0"
+      style={{ backgroundImage: `url(${Bg})` }}
     >
       <div className='flex xl:py-10 py-5 flex-col items-center justify-center bg-center bg-[url("./assets/Tecs_icons/tec_bg/bg-desktop.png")] md:bg-none '>
-        <div className="flex flex-row items-center py-10 xl:py-15">
+        <div className="flex flex-row items-center py-10 xl:py-12.5">
           <img src={star} alt="start-icon" className="w-12" />
           <h2 className="text-[24px] font-chakra font-bold tracking-[4%]">
             Tecnologias usadas
           </h2>
         </div>
 
-        <div className="flex flex-col items-center justify-center xl:scale-90 md:flex-row md:w-[90%] xl:w-[85%] flex-wrap gap-5 lg:gap-y-12.5">
+        <div className="flex flex-col items-center justify-center xl:scale-90 md:flex-row md:w-[90%] xl:w-[85%] xxl:scale-100 flex-wrap gap-5 xl:pb-20 lg:gap-y-12.5">
           {tecData.map((item) => (
             <MiniCard
               key={item.id}
